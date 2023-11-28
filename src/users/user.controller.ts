c26bf9cdb8aefb310119be82bf93d6aeab6cb670
Leo Souza <leonardodomaria@gmail.com>
2023-11-28 14:59:29 -0300

    prisma db

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,9 +1,38 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { randomUUID } from 'crypto';
 
-@Controller()
+type ParamsUser = {
+  id: string;
+  idEmpresa: string;
+};
+
+type QueryUser = {
+  p: string;
+  r: string;
+};
+
+type BodyUser = {
+  name: string;
+  idade: number;
+};
+
+@Controller('users')
 export class UserController {
-  @Get('/user/:id')
-  findById(@Param() params: string) {
-    return;
+  @Get('/:id/:idEmpresa')
+  findById(@Param() params: ParamsUser) {
+    return 'Usuario do ID ' + params.id + '- Empresa ID ' + params.idEmpresa;
+  }
+
+  @Get('/findByPages')
+  findByPages(@Query() queries: QueryUser) {
+    return 'Queries ' + queries.p;
+  }
+
+  @Post('/create')
+  create(@Body() body: BodyUser) {
+    return {
+      ...body,
+      id: randomUUID(),
+    };
   }
 }
